76b0ae37020e8c4efb571eabf7f9f23f31509cd7
Alyssasorensen <-m>
2023-11-09 04:13:58 +0000

    updated geocoding.py file

--- geocoding_gcp/geocoding.py
+++ geocoding_gcp/geocoding.py
@@ -1,7 +1,4 @@
-## GCP
-# https://developers.google.com/maps/documentation/geocoding/start
-# https://developers.google.com/maps/documentation/geocoding/requests-reverse-geocoding
-
+# Install and Import Packages
 import requests
 import pandas as pd
 import numpy as np
@@ -10,30 +7,62 @@ import geopandas as gpd
 import matplotlib.pyplot as plt
 import urllib.parse
 import os
-from dotenv import load_dotenv
+import requests
+import time
+import pandas as pd
+from geopy.geocoders import GoogleV3
 
-load_dotenv()
+# pip install -q -U googlemaps
+import googlemaps
+import pandas as pd
 
-search = 'https://maps.googleapis.com/maps/api/geocode/json?address='
-location = 'New York City'
-api_key = os.getenv("GOOGLE_MAPS_API")
+# Authenticate with Google Maps API with API Key
+api_key = "AIzaSyBqpXVgyXNwPZZx71Gzgtpg_tf3GFWcoMQ"
+gmaps = googlemaps.Client(key=api_key)
 
-# convert location to url friendly string
-location = urllib.parse.quote(location)
-url = search + location + '&key=' + api_key
+# Load in the Datasets
+# Load the dataset
+addresses_df = pd.read_csv('https://raw.githubusercontent.com/Alyssasorensen/datasci_7_geospatial/main/datasets/fulldata_assignment7_slim_hospital_addresses.csv')
+addresses_df
 
-# get response
-response = requests.get(url)
+# Load the dataset
+coordinates_df = pd.read_csv('https://raw.githubusercontent.com/Alyssasorensen/datasci_7_geospatial/main/datasets/fulldata_assignment7_slim_hospital_coordinates.csv')
+coordinates_df
 
-# get json
-json = response.json()
+# Randomly select 100 rows from each dataset
+addresses_sample = addresses_df.sample(n=100, random_state=42)
+addresses_df
 
-results = json['results'][0]
-geometry = results['geometry']
-location = geometry['location']
-lat = location['lat']
-lng = location['lng']
+# Randomly select 100 rows from each dataset
+coordinates_sample = coordinates_df.sample(n=100, random_state=42)
+coordinates_sample
 
+# Geocoding
+geocoded_addresses = []
 
-### create loop, exampple list of data address to go through 
-### and hit the API 
+for index, row in addresses_sample.iterrows():
+    address = row['ADDRESS']
+    geocode_result = gmaps.geocode(address)
+
+    if geocode_result:
+        location = geocode_result[0]['geometry']['location']
+        lat, lng = location['lat'], location['lng']
+        geocoded_addresses.append({'ADDRESS': address, 'Latitude': lat, 'Longitude': lng})
+
+geocoded_df = pd.DataFrame(geocoded_addresses)
+geocoded_df
+
+# Reverse Geocoding 
+reverse_geocoded_addresses = []
+
+for index, row in coordinates_sample.iterrows():
+    lat = row['X']
+    lng = row['Y']
+    reverse_geocode_result = gmaps.reverse_geocode((lat, lng))
+
+    if reverse_geocode_result:
+        address = reverse_geocode_result[0]['formatted_address']
+        reverse_geocoded_addresses.append({'X': lat, 'Y': lng, 'Address': address})
+
+reverse_geocoded_df = pd.DataFrame(reverse_geocoded_addresses)
+reverse_geocoded_df
